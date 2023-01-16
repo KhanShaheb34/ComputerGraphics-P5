6697e513a0e0d54f703161ebd9efc3492fc46936
KhanShaheb34 <shakirulhkhan@gmail.com>
2023-01-17 01:12:31 +0600

    feat: bresenham circle drawing algorithm

---
+++ src/algorithms/circleDrawing/bresenhamCircle.ts
@@ -0,0 +1,27 @@
+import { Display } from '../../lib/display';
+import { plotEightWaySymmetric } from './plotEightWaySymmetric';
+
+export const BresenhamCircle = (
+  display: Display,
+  x0: number,
+  y0: number,
+  radius: number
+) => {
+  let x = 0;
+  let y = radius;
+
+  let decision = 3 - 2 * radius;
+
+  plotEightWaySymmetric(display, x0, y0, x, y);
+
+  while (y >= x) {
+    x++;
+    if (decision > 0) {
+      y--;
+      decision = decision + 4 * (x - y) + 10;
+    } else {
+      decision = decision + 4 * x + 6;
+    }
+    plotEightWaySymmetric(display, x0, y0, x, y);
+  }
+};
---
+++ src/algorithms/circleDrawing/plotEightWaySymmetric.ts
@@ -0,0 +1,18 @@
+import { Display } from '../../lib/display';
+
+export const plotEightWaySymmetric = (
+  display: Display,
+  x0: number,
+  y0: number,
+  x: number,
+  y: number
+) => {
+  display.putPixel(x0 + x, y0 + y);
+  display.putPixel(x0 + y, y0 + x);
+  display.putPixel(x0 - y, y0 + x);
+  display.putPixel(x0 - x, y0 + y);
+  display.putPixel(x0 - x, y0 - y);
+  display.putPixel(x0 - y, y0 - x);
+  display.putPixel(x0 + y, y0 - x);
+  display.putPixel(x0 + x, y0 - y);
+};
