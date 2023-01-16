e9f3af798eaee8279bf602ec52ef972ffb9942e0
KhanShaheb34 <shakirulhkhan@gmail.com>
2023-01-17 00:03:59 +0600

    fix: dda algorithm path

--- src/algorithms/lineDrawing/dda.ts
+++ src/algorithms/lineDrawing/dda.ts
@@ -1,4 +1,4 @@
-import { Display } from '../lib/display';
+import { Display } from '../../lib/display';
 
 export const DDA = (
   display: Display,
@@ -9,7 +9,7 @@ export const DDA = (
 ) => {
   const dx = x2 - x1;
   const dy = y2 - y1;
-  const steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
+  const steps = Math.max(Math.abs(dx), Math.abs(dy));
   const xInc = dx / steps;
   const yInc = dy / steps;
   let x = x1;
