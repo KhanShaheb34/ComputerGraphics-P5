e0fa634b1170ec2f04f9de7f14038d107375b4fa
KhanShaheb34 <shakirulhkhan@gmail.com>
2023-01-17 00:52:39 +0600

    fix: remove unnecessary code form bresenham line

--- src/algorithms/lineDrawing/bresenhamLine.ts
+++ src/algorithms/lineDrawing/bresenhamLine.ts
@@ -7,15 +7,14 @@ export const BresenhamLine = (
   x2: number,
   y2: number
 ) => {
-  display.putPixel(x1, y1, 'red');
-  display.putPixel(x2, y2, 'red');
-
   let m = (y2 - y1) / (x2 - x1);
 
+  // Handling negative slope lines
   if (m < 0) {
     [y1, y2] = [display.resolution - y1, display.resolution - y2];
   }
 
+  // Handling lines where x1 > x2
   if (x1 > x2) {
     [x1, x2] = [x2, x1];
     [y1, y2] = [y2, y1];
@@ -28,9 +27,6 @@ export const BresenhamLine = (
   let p = dx < dy ? 2 * dx - dy : 2 * dy - dx;
   let pInc1 = dx < dy ? 2 * dx : 2 * dy;
   let pInc2 = dx < dy ? 2 * (dx - dy) : 2 * (dy - dx);
-
-  console.log(dx, dy);
-  console.log(x1, y1, x2, y2);
 
   while (x <= x2 && y <= y2) {
     display.putPixel(x, m < 0 ? display.resolution - y : y);
