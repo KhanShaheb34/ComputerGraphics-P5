b1ec462a155f05353b3b811c9819deb0adc9927d
KhanShaheb34 <shakirulhkhan@gmail.com>
2023-01-17 10:55:09 +0600

    exam: clean canvas

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
-import { fillOnMouseClick } from '../algorithms/filling/regionFilling';
-import { PolygonDrawing } from '../algorithms/polygonDrawing/polygonDrawing';
-import { RectanglePoints } from '../algorithms/polygonDrawing/rectangleExample';
-import { Reflect } from '../algorithms/transformation/reflect';
-import { Rotate } from '../algorithms/transformation/rotate';
-import { Scale } from '../algorithms/transformation/scale';
-import { Translate } from '../algorithms/transformation/translate';
 import { Display } from './lib/display';
 import './style.css';
 import P5 from 'p5';
@@ -23,32 +16,7 @@ const sketch = (p5: P5) => {
 
   p5.draw = () => {
     display.draw(p5);
-    // DDA(display, 10, 12, 35, 42);
-    // BresenhamLine(display, 10, 90, 80, 10);
-    // BresenhamCircle(display, 0, 0, 15);
-    // MidPointCircle(display, 0, 0, 5);
-
-    // CohenSutherland(display, 10, 12, 35, 42);
-    // CohenSutherland(display, 10, 30, 95, 42);
-    // CohenSutherland(display, 15, 85, 65, 90);
-    // CohenSutherland(display, 50, 95, 95, 30);
-
-    PolygonDrawing(display, RectanglePoints);
-
-    PolygonDrawing(
-      display,
-      Translate(
-        Reflect(
-          Scale(Rotate(Translate(RectanglePoints, -50, -45), 45), 1.2, 1.5),
-          'y'
-        ),
-        50,
-        45
-      )
-    );
   };
-
-  p5.mousePressed = () => fillOnMouseClick(display, p5, GRID_SIZE, W);
 };
 
 new P5(sketch);
